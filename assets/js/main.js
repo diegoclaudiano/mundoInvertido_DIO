8a645a04787a0d59aac19749ec2b3de4173d24b8
Diêgo Claudiano <dgsilva.230493@gmail.com>
2022-08-26 17:15:33 -0300

    javascript parte 2

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+
+const txtName = document.getElementById('txtName');
+const txtEmail = document.getElementById('txtEmail');
+const txtLevel = document.getElementById('txtLevel');
+const txtCharacter = document.getElementById('txtCharacter');
+const btnSubscribe = document.getElementById('btnSubscribe');
+
+debugger
+
+console.log(txtName)
